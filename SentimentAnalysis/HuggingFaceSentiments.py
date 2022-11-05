6eefcb536a2857c1ec8663f22c5c67223f981218
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2022-11-05 19:22:10 +0200

    runOnGpu

---
+++ SentimentAnalysis/HuggingFaceSentiments.py
@@ -0,0 +1,16 @@
+from transformers import pipeline
+import torch
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+from datasets import load_dataset
+
+imdb = load_dataset("imdb")
+
+# use only part of dataset
+small_train_dataset = imdb["train"].shuffle(seed=42).select([i for i in list(range(3000))])
+small_test_dataset = imdb["test"].shuffle(seed=42).select([i for i in list(range(300))])
+
+# preprocess data using AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+

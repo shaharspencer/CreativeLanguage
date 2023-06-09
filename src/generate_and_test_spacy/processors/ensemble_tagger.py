617184f7d42d20b3a680e350b0a45acac8a138b9
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-06-10 02:04:00 +0300

    some prolem with morph but rest works

---
+++ src/generate_and_test_spacy/processors/ensemble_tagger.py
@@ -0,0 +1,175 @@
+# for BERT tokenizer
+import concurrent
+import stanza
+
+from spacy import Language
+from transformers import BertTokenizer, BertForTokenClassification
+import torch
+import spacy
+from spacy.tokens import Doc
+
+# to execute code concurrently
+from concurrent.futures import ThreadPoolExecutor
+
+# for NLTK tokenizer
+import nltk
+import flair
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+
+#TODO exchange to universal dependencies
+
+
+# @Language.component("custom_tagger")
+# def custom_tagger(doc):
+#     tagged_tokens = nltk.pos_tag([token.text for token in doc])
+#     for token, (text, tag) in zip(doc, tagged_tokens):
+#         token.tag_ = tag
+#     return doc
+
+
+class EnsembleTagger():
+    def __init__(self):
+        # bert dependencies
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.model = BertForTokenClassification.from_pretrained(
+            'bert-base-uncased')
+
+        # nltk dependencies
+        nltk.download('averaged_perceptron_tagger')
+        nltk.download('universal_tagset')
+
+        # stanza dependencies TODO confirm these are UD
+        stanza.download('en')
+        self.stanza_pipeline = stanza.Pipeline('en', processors='tokenize,mwt,pos',
+                                               tokenize_pretokenized=True)
+        # flair dependency (UD pipeline)
+        self.flair_pipeline = SequenceTagger.load("flair/upos-english")
+
+        self.tagger_funcs = [self.flair_tokenizer, self.nltk_tagger,self.stanza_tagger]
+
+
+
+    """
+        predicts the labels for a sentence based on the majority vote
+        of the chosen models. 
+        @:param doc 
+    """
+    def get_tags_list(self, doc):
+        votes =  self.get_all_votes(doc)
+
+        tags = self.calculate_votes(votes) #TODO make this a doc object
+        return tags
+
+
+    def calculate_votes(self, votes: [list[list]]):
+        votes = self.create_lists_from_elements(votes)
+        polled_results = {}
+        with ThreadPoolExecutor() as executor:
+            tagging_tasks = [executor.submit(self.majority_vote, [vote, index]) for index,
+                             vote in enumerate(votes)]
+        # Retrieve results from completed tasks
+        for task in concurrent.futures.as_completed(tagging_tasks):
+            result = task.result()
+            polled_results[result[0]] = result[1]
+        return polled_results
+
+
+    def get_all_votes(self, doc):
+        tagged_tokens_lst = []
+        spacy_tokens = [token.text for token in doc]
+        with ThreadPoolExecutor() as executor:
+            tagging_tasks = [executor.submit(tagger_func, spacy_tokens) for
+                             tagger_func
+                             in self.tagger_funcs]
+
+        # Retrieve results from completed tasks
+        for task in concurrent.futures.as_completed(tagging_tasks):
+            result = task.result()
+            tagged_tokens_lst.append(result)
+
+        return tagged_tokens_lst
+
+
+    def create_lists_from_elements(self,list_of_lists):
+        result_lists = []
+        transposed = zip(*list_of_lists)
+
+        for elements in transposed:
+            result_lists.append(list(elements))
+
+        return result_lists
+
+
+        # for token, (text, tag) in zip(doc, tagged_tokens):
+        #         token.tag_ = tag
+        #     return doc
+        #
+
+
+    """
+    finds the tag with maximum votes for some token
+    @:param tags potentials tags for some token
+    @:return tag the tag that received the majority vote
+    """
+    def majority_vote(self, tags_and_index):
+        tag_counts = {}
+        tags = tags_and_index[0]
+        index = tags_and_index[1]
+        for vote in tags:
+            tag = vote[1]
+            if tag in tag_counts:
+                tag_counts[tag] += 1
+            else:
+                tag_counts[tag] = 1
+        majority_tag = max(tag_counts, key=tag_counts.get)
+        return [index, [tags[0][0], majority_tag]]
+
+    # """
+    #     predicts the labels for the tokens according to BERT model
+    #     @:param doc to tag tokens in
+    #     @:param tagged_tokens_list list to add our predictions to
+    #     @:return tags for tokens
+    # """
+    # def bert_tagger(self, doc: Doc):
+    #     text = [token.text for token in doc]
+    #     tokens = self.tokenizer.encode(text, add_special_tokens=True)
+    #     input_ids = torch.tensor([tokens])
+    #     with torch.no_grad():
+    #         outputs = self.model(input_ids)
+    #         predictions = outputs[0].argmax(dim=2).squeeze().tolist()
+    #     tags = [self.tokenizer.convert_ids_to_tokens(pred) for pred in predictions][
+    #            1:-1]
+    #     return tags
+
+    """
+          predicts the labels for the tokens based on stanza model
+          @:param doc to tag tokens in 
+      """
+    def stanza_tagger(self, doc): #TODO represents PUNCT as "."
+        # doc = [token.text for token in doc]
+
+        taggings = self.stanza_pipeline([doc])
+        tags = [[word.text, word.upos] for word in taggings.sentences[0].words]
+
+        return tags
+
+
+
+    """
+        predicts the labels for the tokens based on NLTK model
+        @:param doc to tag tokens in 
+    """
+    def nltk_tagger(self, tokens):
+         tagged_tokens = nltk.pos_tag(tokens, tagset='universal')
+
+         return tagged_tokens
+
+
+    def flair_tokenizer(self, token_list:list[str]):
+        # since the text is pretokenized, create a sentence object out of it
+        sentence = Sentence(token_list)
+        self.flair_pipeline.predict(sentence)
+        tags = [[token.text, token.labels[0].value] for token in sentence]
+        return tags

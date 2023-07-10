b4bb395d04a392c942ad2437e3b70767262fbe04
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-07-10 17:01:29 +0300

    changed back to roberta token replacements

--- src/extract_creative_sentences_by_dim/Embeddings/bert.py
+++ src/extract_creative_sentences_by_dim/Embeddings/bert.py
@@ -4,23 +4,34 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import spacy
-from transformers import pipeline, BertTokenizer, TFBertForMaskedLM
+from transformers import DebertaTokenizer, DebertaModel
+from transformers import pipeline
 from src.generate_and_test_spacy.processors.processor import Processor
 
-#TODO use multiprocessing
+
+# TODO use multiprocessing
 
 
-class BertConverter: # TODO better name
-
+class FillMask:
     """
         initialize tokenizer and model instances
     """
+
     def __init__(self):
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
-        self.model = TFBertForMaskedLM.from_pretrained('bert-base-cased')
-        # self.processor = processor.Processor(to_process=False)
+        # self.tokenizer = DebertaTokenizer.from_pretrained(
+        #     "microsoft/deberta-v3-base")
+        # self.model = DebertaModel.from_pretrained(
+        #     "microsoft/deberta-v3-base")
+
+        self.classifier = pipeline("fill-mask", "xlm-roberta-large", top_k=5)
         self.nlp = spacy.load("en_core_web_lg")
         self.processor = Processor(to_process=False, to_conllu=False)
+
+        text = "I love my <mask>."
+
+        predicted_tokens = self.classifier(text)
+        for token in predicted_tokens:
+            print(token)
 
     """
         returns top k predictions to replace [MASK] token
@@ -32,31 +43,38 @@ class BertConverter: # TODO better name
                                         ensemble_tagger
         @:return input_dataframe(pf.DataFrame): dataframe with two columns 
                                                 added - 
-                                                "POS PREDICTIONS",
-                                                "BERT REPLACEMENTS"
+                                                "ROBERTA POS PREDICTIONS",
+                                                "ROBERTA REPLACEMENTS"
     """
-    def get_top_k_predictions(self, input_dataframe: pd.DataFrame, tagger, k=5) \
-                                    -> pd.DataFrame:
+
+    def get_top_k_predictions(self, input_dataframe: pd.DataFrame, tagger) \
+            -> pd.DataFrame:
         replacement_pos_predictions = []
         replacement_word_predictions = []
         for index, row in input_dataframe.iterrows():
-            replacements, pos_prediction = self.__predict_single_row(row, k, tagger)
+            replacements, pos_prediction = self.__predict_single_row(row,
+                                                                     tagger)
             replacement_pos_predictions.append(pos_prediction)
             replacement_word_predictions.append(replacements)
-        self.__append_outputs_to_csv(input_dataframe, replacement_pos_predictions,
+        self.__append_outputs_to_csv(input_dataframe,
+                                     replacement_pos_predictions,
                                      replacement_word_predictions)
         return input_dataframe
 
     """
-        adds the outputs of the BERT predictions and the new pos prediction
+        adds the outputs of the ROBERTA predictions and the new pos prediction
         to the analyzed dataframe.
         @:param input_dataframe(
     """
+
     def __append_outputs_to_csv(self, input_dataframe,
                                 replacement_pos_predictions: list[str],
-                                     replacement_word_predictions: list[list[str]]) -> None:
-        input_dataframe["POS PREDICTIONS"] = pd.Series(replacement_pos_predictions)
-        input_dataframe["BERT REPLACEMENTS"] = pd.Series(replacement_word_predictions)
+                                replacement_word_predictions: list[
+                                    list[str]]) -> None:
+        input_dataframe["POS PREDICTIONS"] = pd.Series(
+            replacement_pos_predictions)
+        input_dataframe["ROBERTA REPLACEMENTS"] = pd.Series(
+            replacement_word_predictions)
 
     """
         return tuple of potential replacements and predicted ensemble pos.
@@ -68,26 +86,28 @@ class BertConverter: # TODO better name
         @:return replacements, pos_predictions(tuple[list[str], str]):
                     potential replacements and pos ensemble prediction on 
                     replacements
-    
+
     """
-    def __predict_single_row(self, row: pd.Series, k: int, tagger: str)\
-                                -> tuple[list[str], str]:
+
+    def __predict_single_row(self, row: pd.Series, tagger: str) \
+            -> tuple[list[str], str]:
         row_text = list(copy.deepcopy(row["tokenized sentence"]))
         try:
             masked_tokenized_sentence = self.replace_with_token(row_text,
-                                                            row["token index"],
-                                                            replace_with="[MASK]"
-                                                            )
+                                                                row[
+                                                                    "index of verb"],
+                                                                replace_with="<mask>"
+                                                                )
         except Exception:
             return ([""], "")
         replacements = self.__get_top_k_replacements(
-            " ".join(masked_tokenized_sentence), k=k)
-        replacements = replacements.split(" ")
-        pos_predictions = self.__get_top_k_pos_predictions(replacements, tagger=tagger,
+            " ".join(masked_tokenized_sentence))
+        pos_predictions = self.__get_top_k_pos_predictions(replacements,
+                                                           tagger=tagger,
                                                            sentence=row_text,
                                                            index=row[
-                                                               "token index"]
-                                                          )
+                                                               "index of verb"]
+                                                           )
         return replacements, pos_predictions
 
     """
@@ -101,9 +121,11 @@ class BertConverter: # TODO better name
                                 whether we want to use a regular spacy tagger
                                 or the ensemble tagger
         @:return majority pos prediction for replacement
-        
+
     """
-    def __get_top_k_pos_predictions(self, replacements, sentence, index, tagger):
+
+    def __get_top_k_pos_predictions(self, replacements, sentence, index,
+                                    tagger) -> str:
         pos_dict = defaultdict(int)
         for replacement in replacements:
 
@@ -123,30 +145,20 @@ class BertConverter: # TODO better name
 
         return max(pos_dict, key=pos_dict.get)
 
-
-
     """
         returns top k replacemnents for [MASK] token.
         @:param input_string(str): string to replace [MASK] token in.
         @:param k(int): how many replacements to generate
         @:return TODO
     """
-    def __get_top_k_replacements(self, input_string:str, k):
-        tokenized_inputs = self.tokenizer(input_string,
-                                          return_tensors="tf")
-        outputs = self.model(tokenized_inputs["input_ids"])
 
-        top_k_indices = tf.math.top_k(outputs.logits, k).indices[0].numpy()
-        decoded_output = self.tokenizer.batch_decode(top_k_indices)
-        mask_token = self.tokenizer.encode(self.tokenizer.mask_token)[1:-1]
-        mask_index = \
-            np.where(tokenized_inputs['input_ids'].numpy()[0] == mask_token)[0][0]
-
-        decoded_output_words = decoded_output[mask_index]
-        return decoded_output_words
+    def __get_top_k_replacements(self, input_string: str) -> list[str]:
+        replacements = self.classifier(input_string)
+        replacement_tokens = [entry["token_str"] for entry in replacements]
+        return replacement_tokens
 
     def replace_with_token(self, tokenized_sent: list[str], token_index: int,
-                           replace_with: str):
+                           replace_with: str) -> list[str]:
         sent = copy.deepcopy(tokenized_sent)
         if token_index >= len(sent):
             raise Exception("list index out of range")
@@ -155,21 +167,37 @@ class BertConverter: # TODO better name
 
 
 if __name__ == '__main__':
-    bert = BertConverter()
+    fill_mask = FillMask()
+
     dtypes = {
         'lemma': str,
         'word form': str,
         'sentence': str,
         'doc index': int,
         'sent index': int,
-        'token index': int,
+        'index of verb': int,
         'tokenized sentence': str
     }
 
-    df = pd.read_csv(
-        r"roberta_regulartagger - roberta_regulartagger.csv",
-    encoding="ISO-8859-1",
-        dtype=dtypes, converters={'tokenized sentence': eval},
-        on_bad_lines="skip"
-    )
-    bert.get_top_k_predictions(df, tagger = "REGULAR")
+    df_list = [
+        r"morph_order_by_entropy_and_verb_perc2023_06_19_with_BERT.csv"]
+
+    # opened_df_zero = pd.read_csv(df_list[0],
+    # encoding="ISO-8859-1",
+    #     dtype=dtypes, converters={'tokenized sentence': eval},
+    #     on_bad_lines="skip"
+    #  )
+    #
+    # new_df_ensemble = fill_mask.get_top_k_predictions(opened_df_zero,"ENSEMBLE")
+    #
+    # new_df_ensemble.to_csv("roberta_ensembletagger.csv")
+
+    opened_df_one = pd.read_csv(df_list[0],
+                                encoding="ISO-8859-1",
+                                dtype=dtypes,
+                                converters={'tokenized sentence': eval},
+                                on_bad_lines="skip"
+                                )
+
+    new_df_regular = fill_mask.get_top_k_predictions(opened_df_one, "REGULAR")
+    new_df_regular.to_csv("roberta_regulartagger.csv")

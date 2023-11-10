bacb2146ed4644200610ce30ec03ccbce9e86564
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-11-10 14:43:03 +0200

    added tagging with spacy

--- src/masking_subproject/tagging/tag_with_spacy.py
+++ src/masking_subproject/tagging/tag_with_spacy.py
@@ -1,12 +1,18 @@
 import spacy
 from conllu import parse
+from docopt import docopt
 
-# Load the spaCy model
-nlp = spacy.load("en_core_web_sm")
+usage = '''
+word_pos_converter CLI.
+number of 
+Usage:
+    word_pos_converter.py <file_to_proccess> <n_sentences>
+'''
 
-# Function to convert CoNLL-U format to tagged tokens
-def convert_conllu_to_tagged_text(conllu_content, output_file):
+def convert_conllu_to_tagged_text(conllu_content, output_file,
+                                  sentence_limit):
     with open(output_file, 'w', encoding='utf-8') as f:
+        sentence_count = 0
         for sentence in conllu_content:
             sentence_text = " ".join([token_info['form'] for token_info in sentence])
             doc = nlp(sentence_text)
@@ -15,21 +21,24 @@ def convert_conllu_to_tagged_text(conllu_content, output_file):
                 f.write(f"{token.text} {token.pos_}\n")
 
             f.write("\n")
+            sentence_count += 1
 
-# Replace 'your_input_file.conllu' and 'output_tagged_text.txt' with your file paths
-
+            if sentence_limit is not None and sentence_count >= sentence_limit:
+                break
 
 if __name__ == '__main__':
     args = docopt(usage)
     file_to_process = args["<file_to_proccess>"]
     n_sentences = int(args["<n_sentences>"])
-    input_file = 'your_input_file.conllu'
-    output_file = 'output_tagged_text.txt'
 
-    with open(input_file, 'r', encoding='utf-8') as conllu_file:
+    output_file = '../files/output_with_pos_SPACY_tags_10_sentences.txt'
+
+    nlp = spacy.load("en_core_web_lg")
+
+    with open(file_to_process, 'r', encoding='utf-8') as conllu_file:
         conllu_content = parse(conllu_file.read())
 
-    convert_conllu_to_tagged_text(conllu_content, output_file)
+    convert_conllu_to_tagged_text(conllu_content, output_file,
+                                  sentence_limit=n_sentences)
 
-    print(f'Data converted and saved to {output_file} with spaCy POS tags')
-    pass
+    print(f'data converted and saved to {output_file} with spaCy POS tags')

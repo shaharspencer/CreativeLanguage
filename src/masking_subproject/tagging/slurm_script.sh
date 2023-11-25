92e3064b334c9461b6583475923316a04f0762fb
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2023-11-25 19:00:59 +0200

    some bug in masking_algorithm.py

--- src/masking_subproject/tagging/slurm_script.sh
+++ src/masking_subproject/tagging/slurm_script.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
+#!/bin/bash
 #SBATCH --gres=gpu:a5000:1  # Replace <number_of_gpus> with the number of GPUs you need
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=10
-#SBATCH --time=2:00:00           # Set the maximum time your job needs to run (e.g., 1:00:00 for one hour)
+#SBATCH --time=40:00:00           # Set the maximum time your job needs to run (e.g., 1:00:00 for one hour)
 #SBATCH --job-name=my_gpu_job
 #SBATCH --output=processor_output.out     # Custom output file for standard output
-#SBATCH --error=processor_output.err      # Custom output file for standard error
-#SBATCH --exclude=<node_hostname>        # Replace <node_hostname> with the hostname of the node you want to exclude
+#SBATCH --error=processor_output.out     # Custom output file for standard error
+#SBATCH --mem=64GB          # Request 64 GB of memory
 
 # Load required CUDA module
 
